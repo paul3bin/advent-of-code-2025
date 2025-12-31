41ba41c6de691167eb59904773783d40273dac72
pau3bin <ebinpaul@outlook.com>
2025-12-31 12:14:43 +0000

    WIP: day1 part2 setup

--- day1/main.go
+++ day1/main.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 )
 
-func get_input_array() ([]string) {
+func get_input_array() []string {
 	data, err := os.ReadFile("input.txt")
 
-	if (err != nil){
+	if err != nil {
 		panic(err)
 	}
 
@@ -19,26 +19,53 @@ func get_input_array() ([]string) {
 	return inputs
 }
 
-func main(){
+func part1() {
 	inputs := get_input_array()
 	dial_pointer := 50
 	password := 0
 
-	for _, input := range inputs{
-		if string(input[0]) == "L"{
-			num, _ := strconv.Atoi(string(input[1:]))
-			dial_pointer -= num 
-			} else if (string(input[0]) == "R"){
-				num, _ := strconv.Atoi(string(input[1:]))
-				dial_pointer += num
+	for _, input := range inputs {
+		num, _ := strconv.Atoi(string(input[1:]))
+		if string(input[0]) == "L" {
+			dial_pointer -= num
+		} else if string(input[0]) == "R" {
+			dial_pointer += num
 		}
 
 		dial_pointer %= 100
-		
-		if dial_pointer == 0{
+
+		if dial_pointer == 0 {
 			password += 1
 		}
 	}
 
 	fmt.Println(password)
+}
+
+func part2() {
+	inputs := get_input_array()
+	dial_pointer := 50
+	password := 0
+
+	for _, input := range inputs {
+		num, _ := strconv.Atoi(string(input[1:]))
+		password += int(num / 100)
+		if string(input[0]) == "L" {
+			dial_pointer -= num
+		} else if string(input[0]) == "R" {
+			dial_pointer += num
+		}
+		dial_pointer %= 100
+
+		if dial_pointer == 0 {
+			password += 1
+		}
+	}
+
+	fmt.Println(password)
+}
+
+func main() {
+	part1()
+	part2()
 }
