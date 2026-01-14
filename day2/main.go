e8510b7f9c725107e51466b5a5da0b4bc4c515f4
pau3bin <ebinpaul@outlook.com>
2026-01-14 23:22:45 +0000

    feat: day2 solutions

--- day2/main.go
+++ day2/main.go
@@ -9,7 +9,6 @@ import (
 
 func getInputArray() []string {
 	data, err := os.ReadFile("input.txt")
-	// data, err := os.ReadFile("test_input.txt")
 
 	if err != nil {
 		panic(err)
@@ -38,7 +37,34 @@ func getInvalidIdsP1(inputs []string) int {
 	return result
 }
 
+func hasRepeatedSubstring(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	ss := s + s
+	return strings.Contains(ss[1:len(ss)-1], s)
+}
+
+func getInvalidIdsP2(inputs []string) int {
+	result := 0
+
+	for _, input := range inputs {
+		idRange := strings.Split(input, "-")
+		start, _ := strconv.Atoi(string(idRange[0]))
+		stop, _ := strconv.Atoi(string(idRange[1]))
+		for i := start; i <= stop; i++ {
+			numString := strconv.Itoa(i)
+			if hasRepeatedSubstring(numString) {
+				result += i
+			}
+		}
+	}
+
+	return result
+}
+
 func main() {
 	inputs := getInputArray()
 	fmt.Println(getInvalidIdsP1(inputs))
+	fmt.Println(getInvalidIdsP2(inputs))
 }
